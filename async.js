644d558f15dad97258575a9d7cf92950a43c4770
soilflux <52200608+soilflux@users.noreply.github.com>
2026-02-06 13:44:22 -0500

    add responses to loaded text, dont repeat conversations

--- async.js
+++ async.js
@@ -2,7 +2,9 @@ if (localStorage.getItem("chatterID")) {document.getElementById("chatterID").val
 
 const fileInput = document.getElementById('txtPicker');
 let sortedMatrix = Array.from({ length: 100 }, () => []);
+let rawFileContent = "";
 let pickedFile = false;
+let conversationID = -1;
 
 fileInput.addEventListener('change', (event) => {
     const file = event.target.files[0]; // Get the selected file
@@ -10,9 +12,9 @@ fileInput.addEventListener('change', (event) => {
     if (file) {
         const reader = new FileReader();
         
-        reader.onload = function(e) {
-            let content = e.target.result;
-            content = content.split(":::");
+        reader.onload = function(e) {   
+            rawFileContent = e.target.result;
+            let content = rawFileContent.split(":::");
             for (var i = 0; i < content.length; i++) {
               for (var j = 0; j < 100; j++) {
                 if (content[i].startsWith(j)) {
@@ -25,21 +27,26 @@ fileInput.addEventListener('change', (event) => {
         document.getElementById("txtPicker").style.display = "none";
         document.getElementById("getNextConversation").style.display = "inline-block";
         pickedFile = true;
-        setTimeout(getNextConversation,1);
+        setTimeout(getNextConversation,10);
         
     }
 });
 
 function getNextConversation() {
+  console.log(rawFileContent)
+  if (conversationID != -1) {
+    rawFileContent += ":::" + conversationID + document.getElementById("chatterID").value + " " + document.getElementById("response").innerText + "\n";
+  }
+  console.log(rawFileContent)
   const listContainer = document.getElementById("currentConversation");
   
   // 1. Clear the last conversation immediately
   listContainer.innerHTML = ""; 
 
-  for (var j = 0; j < 100; j++) {
+  for (var j = conversationID+1; j < 100; j++) {
     console.log(sortedMatrix);
     if (sortedMatrix[j][sortedMatrix[j].length-1].startsWith("s ")) {
-      
+      conversationID = j;
       sortedMatrix[j].forEach((item) => {
         let li = document.createElement("li");
         li.innerText = item;
